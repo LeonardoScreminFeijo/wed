96ca59eb9f7f26b92c4388024c6c42133148d2da
LeonardoScreminFeijo <leofeijo2001@hotmail.com>
2026-04-07 10:48:41 -0300

    Agora vai

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,28 +1,25 @@
 export function configurarBotaoCalendario() {
-  const btnCalendario = document.getElementById("btn-calendario");
+  const btnCalendario = document.getElementById("btn-calendar");
 
   if (!btnCalendario) return;
 
-  btnCalendario.addEventListener("click", (event) => {
-    event.preventDefault();
+  const titulo = "Casamento Ana & Léo";
+  const detalhes =
+    "Venha celebrar o nosso amor! Mal podemos esperar para viver este momento convosco.";
+  const local = "Chácara Doce Recanto | Curitiba, PR";
 
-    const titulo = "Casamento Ana & Léo";
-    const detalhes =
-      "Venha celebrar o nosso amor! Mal podemos esperar para viver este momento convosco.";
-    const local = "Sítio das Flores, São Paulo - SP";
+  const dataInicio = "20270424T190000Z";
+  const dataFim = "20270425T030000Z";
 
-    const dataInicio = "20270424T190000Z";
-    const dataFim = "20270425T030000Z";
+  const isIOS =
+    /iPad|iPhone|iPod/.test(navigator.userAgent) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
 
-    const isIOS =
-      /iPad|iPhone|iPod/.test(navigator.userAgent) ||
-      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
-
-    if (isIOS) {
-      window.location.href = "/convite.ics";
-    } else {
-      const urlGoogle = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(titulo)}&details=${encodeURIComponent(detalhes)}&location=${encodeURIComponent(local)}&dates=${dataInicio}/${dataFim}`;
-      window.open(urlGoogle, "_blank");
-    }
-  });
+  if (isIOS) {
+    btnCalendario.href = "/convite.ics";
+  } else {
+    const urlGoogle = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(titulo)}&details=${encodeURIComponent(detalhes)}&location=${encodeURIComponent(local)}&dates=${dataInicio}/${dataFim}`;
+    btnCalendario.href = urlGoogle;
+    btnCalendario.target = "_blank"; // Abre numa nova aba limpa
+  }
 }
